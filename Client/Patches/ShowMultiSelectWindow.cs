ade9f66ccb70dc74d8fe5b04c50548133079af3b
egbog <3422424+egbog@users.noreply.github.com>
2026-06-10 15:40:10 -0700

    create plugin

---
+++ Client/Patches/ShowMultiSelectWindow.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using SPT.Reflection.Patching;
+
+namespace _OpenBarters.Patches;
+public class ShowMultiSelectWindow : ModulePatch
+{
+    protected override MethodBase GetTargetMethod() {
+        return typeof(RepairControllerClass).GetMethod("method_1", BindingFlags.Instance | BindingFlags.Public);
+    }
+}
---
+++ Client/Plugin.cs
@@ -0,0 +1,25 @@
+/* LICENSE:
+ * MIT
+ *
+ * AUTHOR:
+ * egbog
+ * */
+
+using _OpenBarters.Patches;
+using BepInEx;
+using BepInEx.Logging;
+
+namespace _OpenBarters;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInProcess("EscapeFromTarkov.exe")]
+public class Plugin : BaseUnityPlugin {
+    public static readonly ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource("OpenBarters");
+
+    private void Awake() {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+        new ShowMultiSelectWindow().Enable();
+    }
+}
